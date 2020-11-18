7ddce48990e1ec5f98e7b0457e04266f44661391
trudova <liubovtrudova@gmail.com>
2020-11-18 15:41:51 -0500

    spam program

--- src/test/SendGmail.java
+++ src/test/SendGmail.java
@@ -2,12 +2,20 @@ package test;
 import library.EmailUtility;
 public class SendGmail {
     public static void main(String[] args) {
-        String userName = "liubovtrudova@gmail.com";
-        String passWord = "password";
-        String receiver = "liubovtrudova@gmail.com";
-        String subject="Liv";
-        String text ="I like your sens oh humor";
-        EmailUtility.sendEmails(userName,passWord,receiver,subject,text);
-        System.out.println("done");
+        String userName = ""; //    your gmail address
+        String passWord = ""; // your gmail password
+        String[] receivers = { }; // receiver's email address (can be any domain)
+        String subject = "";  // subject of the email
+        String text = "";  // text message
+
+        int times  = 1;
+        for(int i = 1; i <= times; i++  ){  // how many times you wanna send?
+
+            for( String each : receivers ){ // to how many people you wanna send the email
+                EmailUtility.sendEmails(userName, passWord, each, subject, text);
+            }
+
+        }
+        System.out.println("Completed");
     }
 }
